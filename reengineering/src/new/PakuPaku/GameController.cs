b1ab042f6a4c310c96d4553aa2531c14b17137f8
weberer <weberer@outlook.com>
2019-09-27 17:25:49 -0500

    Adding files back to the repository

---
+++ reengineering/src/new/PakuPaku/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PakuPaku
+{
+    class GameController
+    {
+        private List<Ghost> ghostlist;
+        private const int NUM_GHOSTS = 4;
+
+        //values from PAS file lines 220-229
+        private const int pointsPerLife = 10000;
+        private const int startingLives = 3;
+        private const int startingLevel = 0;
+
+        private const int startingDots = 244;
+        private const int fruitCounter1 = startingDots - 70;
+        private const int fruitCounter2 = fruitCounter1 - 100;
+        private const int elroy = 20;  //I believe this refers to the number of dots paku must eat before blinky speeds up. Source: https://pacman.fandom.com/wiki/Blinky
+        private const int superElroy = 10;
+
+
+        public GameController()
+        {
+            Console.WriteLine("Game controller has been created");
+            ghostlist = new List<Ghost>();
+        }
+
+        //Responsible for setting up the game
+        public void startGame()
+        {
+            PakuPaku paku = new PakuPaku();
+            spawnGhosts();
+
+        }
+
+        //creates the four ghosts for gameplay
+        public void spawnGhosts()
+        {
+            ghostlist.Add(new Ghost(Ghosts.stinky)); //red
+            ghostlist.Add(new Ghost(Ghosts.kinky)); //blue
+            ghostlist.Add(new Ghost(Ghosts.hinky)); //pink
+            ghostlist.Add(new Ghost(Ghosts.blaine)); //orange
+
+        }
+    }
+}
---
+++ reengineering/src/new/PakuPaku/Ghost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PakuPaku
+{
+    public enum Ghosts
+    {
+        stinky, //red
+        kinky, //pink
+        hinky, //blue
+        blaine  //yellow
+
+}
+    class Ghost
+    {
+        public Ghost(Ghosts ghostname)
+        {
+            Console.WriteLine(ghostname.ToString() + " has spawned. Beware!");
+        }
+
+        
+
+       
+    }
+}
