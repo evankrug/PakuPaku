4c9412ad9ea3b14d845b9e3b6b8cc02c32de55c4
Evan Krug <kruge@uwplatt.edu>
2019-10-18 18:56:21 -0500

    location class, gameState enum, ghostState enum

---
+++ reengineering/src/new/PakuJava/src/Model/Location.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Location
+{
+    public int xLoc, yLoc;
+
+    public Location(int xLoc, int yLoc)
+    {
+        this.xLoc = xLoc;
+        this.yLoc = yLoc;
+    }
+}
