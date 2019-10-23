4a706ee1ac7de6851e857141336ac071ed172d14
Evan Krug <kruge@uwplatt.edu>
2019-10-23 15:55:09 -0500

    ????????????

--- reengineering/src/new/PakuJava/src/Model/Blaine.java
+++ reengineering/src/new/PakuJava/src/Model/Blaine.java
@@ -1,19 +1,10 @@
 package Model;
 
-
 public class Blaine extends Ghost
 {
-    private Location loc;
-    private final int STARTING_X = 14;  //starting x and y coordinates of Paku; subject to change
-    private final int STARTING_Y = 11;
     public Blaine()
     {
 
-        loc = new Location(STARTING_X, STARTING_Y);
     }
 
-    @ Override
-    public void move(Direction dir)   {
-
-    }
 }
--- reengineering/src/new/PakuJava/src/Model/Hinky.java
+++ reengineering/src/new/PakuJava/src/Model/Hinky.java
@@ -1,24 +1,10 @@
 package Model;
 
-
 public class Hinky extends Ghost
 {
-    private Location loc;
-    private final int STARTING_X = 14;  //starting x and y coordinates of Paku; subject to change
-    private final int STARTING_Y = 11;
     Public Hinky()
     {
 
-        loc = new Location(STARTING_X, STARTING_Y);
     }
 
-    public HInky()
-    {
-
-    }
-
-    @ Override
-    public void move(Direction dir)   {
-
-    }
 }
