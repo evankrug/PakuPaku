80780b1f0af4a5a10b11f8ab5198249e1a950292
Evan Krug <kruge@uwplatt.edu>
2019-10-28 16:05:14 -0500

    paku location, ghost location, game status added to GameData class

--- reengineering/src/new/PakuJava/src/Model/GameData.java
+++ reengineering/src/new/PakuJava/src/Model/GameData.java
@@ -1,14 +1,79 @@
 package Model;
 
+
+/**
+ * This class holds all data to be sent to UI at any given time
+ */
+
 public class GameData
 {
     GameStatus gameStatus;
-    Location PakuLoc;
+    Location pakuLoc; //x, y location of the paku
+
+    // The ghost locations
+    Location blaineLoc;
+    Location stinkyLoc;
+    Location hinkyLoc;
+    Location kinkyLoc;
 
 
     public GameData()
     {
 
     }
+
+    public void setGameStatus(GameStatus gameStatus) {
+        this.gameStatus = gameStatus;
+    }
+
+    public void setPakuLoc(Location pakuLoc) {
+        this.pakuLoc = pakuLoc;
+    }
+
+    public void setHinkyLocation(Location loc)
+    {
+        this.hinkyLoc = loc;
+    }
+
+    public void setStinkyLocation(Location loc)
+    {
+        this.stinkyLoc = loc;
+    }
+
+    public void setKinkyLocation(Location loc)
+    {
+        this.kinkyLoc = loc;
+    }
+
+    public void setBlaineLocation(Location loc)
+    {
+        this.blaineLoc = loc;
+    }
+
+    public GameStatus getGameStatus() {
+        return gameStatus;
+    }
+
+    public Location getPakuLoc() {
+        return pakuLoc;
+    }
+
+    public Location getBlaineLoc() {
+        return blaineLoc;
+    }
+
+    public Location getStinkyLoc() {
+        return stinkyLoc;
+    }
+
+    public Location getHinkyLoc() {
+        return hinkyLoc;
+    }
+
+    public Location getKinkyLoc() {
+        return kinkyLoc;
+    }
+
+
 
 }
